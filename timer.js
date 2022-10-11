7cc4c59b7b1f1ff4d1cdc4be52522a0586f3ba6d
IronMattGG <noutangel@gmail.com>
2022-10-11 18:14:55 +0200

    uf-silence 0.2

--- timer.js
+++ timer.js
@@ -2,10 +2,12 @@
 // https://github.com/IronMattGG
 // @ironmattgg on socials
 
+var isStopped = true;
+
 function Time() {
-    let past = new Date(2022, 9, 9, 10, 49);
+    let pastDate = new Date(2022, 9, 9, 10, 49);
     let today = new Date();
-    let t = (today - past);
+    let t = (today - pastDate);
 
     let = days = Math.floor(t / (1000 * 60 * 60 * 24));
     let = hours = Math.floor((t / (1000 * 60 * 60)) % 24);
@@ -13,22 +15,33 @@ function Time() {
     let = seconds = Math.floor((t / 1000) % 60);
 
     return {
-        'days': days, 
-        'hours': hours, 
-        'minutes': minutes, 
-        'seconds': seconds
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds,
+        'pastDate': pastDate
     };
 }
 
+
 function startClock() {
-    var clock = document.getElementById('clockdiv');
+    var clock = document.getElementById('divtimer');
     var daysSpan = clock.querySelector('.days');
     var hoursSpan = clock.querySelector('.hours');
     var minutesSpan = clock.querySelector('.minutes');
     var secondsSpan = clock.querySelector('.seconds');
+    var timeTitle = document.getElementById('timeTitle');
 
     function updateClock() {
-        var t = Time();
+
+        if (!isStopped) {
+            var t = Time();
+            timeTitle.innerHTML = 'Il tempo del silenzio di UF';
+        } else {
+            var t = TimerStopped();
+            timeTitle.innerHTML = 'Il tempo del silenzio di UF è durato:';
+        }
+
         daysSpan.innerHTML = t.days;
         hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
         minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
@@ -37,4 +50,27 @@ function startClock() {
     updateClock();
 }
 
-setInterval(startClock, 1000);
+function TimerStopped() {
+    var stoppedDate = new Date(2022, 9, 11, 17, 26);
+    var startDate = Time().pastDate;
+    var eT = stoppedDate - startDate;
+
+    let = days = Math.floor(eT / (1000 * 60 * 60 * 24));
+    let = hours = Math.floor((eT / (1000 * 60 * 60)) % 24);
+    let = minutes = Math.floor((eT / 1000 / 60) % 60);
+    let = seconds = Math.floor((eT / 1000) % 60);
+
+    return {
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds,
+    };
+    updateClock();
+}
+
+if (!isStopped) {
+    setInterval(startClock, 1000);
+} else {
+    TimerStopped();
+}
